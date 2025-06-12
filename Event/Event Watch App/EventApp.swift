6e023ea4585cedbdb1e70c2960673c62afd5711d
Duong Tuan <tuan.duong@enouvo.com>
2025-06-12 17:09:38 +0700

    init project

---
+++ Event/Event Watch App/EventApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventApp.swift
+//  Event Watch App
+//
+//  Created by Duong Tuan on 01/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct Event_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
