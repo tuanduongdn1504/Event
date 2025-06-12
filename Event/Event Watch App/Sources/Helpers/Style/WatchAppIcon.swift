aa38931913d367fb69c55a57fcffe60b75919c19
Duong Tuan <tuan.duong@enouvo.com>
2025-06-12 17:49:49 +0700

    add: enum for Style

---
+++ Event/Event Watch App/Sources/Helpers/Style/WatchAppIcon.swift
@@ -0,0 +1,19 @@
+//
+//  WatchAppIcon.swift
+//  Event Watch App
+//
+//  Created by Duong Tuan on 01/06/2025.
+//
+
+import SwiftUI
+
+enum WatchAppIcon: String, CaseIterable {
+    case logo = "ic_logo"
+    case timeSheet = "ic_timesheet"
+    case actions = "ic_actions"
+    case news = "ic_news"
+    case events = "ic_events"
+    case calendar = "ic_calendar"
+    case circleTick = "ic_circle_tick"
+    case failed = "ic_failed"
+}
---
+++ Event/Event Watch App/Sources/Helpers/Style/WatchAppSize.swift
@@ -0,0 +1,21 @@
+//
+//  WatchAppSize.swift
+//  Event Watch App
+//
+//  Created by Duong Tuan on 01/06/2025.
+//
+
+import Foundation
+
+enum Spacing: CGFloat {
+    case large = 12
+    case medium = 8
+    case small = 6
+    case xSmall = 4
+    case xxSmall = 2
+}
+
+enum Radius: CGFloat {
+    case large = 12
+    case xSmall = 4
+}
