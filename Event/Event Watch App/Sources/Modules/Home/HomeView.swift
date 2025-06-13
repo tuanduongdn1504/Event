1c8f7b1bc9e48eb060b72ab05fadfc821720e646
Duong Tuan <tuan.duong@enouvo.com>
2025-06-13 23:05:05 +0700

    update: HomeView with NavigationStack

--- Event/Event Watch App/Sources/Modules/Home/HomeView.swift
+++ Event/Event Watch App/Sources/Modules/Home/HomeView.swift
@@ -7,8 +7,92 @@
 
 import SwiftUI
 
-struct HomeView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+enum HomeSectionType: Hashable {
+    case timeSheet
+    case actions
+    case news
+    case events
+    
+    var title: String {
+        switch self {
+        case .timeSheet: return "Timesheet"
+        case .actions: return "Actions"
+        case .news: return "New Timesheet"
+        case .events: return "Events"
+        default: return ""
+        }
+    }
+    
+    var icon: String {
+        switch self {
+        case .timeSheet: return WatchAppIcon.timeSheet.rawValue
+        case .actions: return WatchAppIcon.actions.rawValue
+        case .news: return WatchAppIcon.news.rawValue
+        case .events: return WatchAppIcon.events.rawValue
+        default: return ""
+        }
     }
 }
+
+struct HomeView: View {
+    private let homeSections: [HomeSectionType] = [.timeSheet, .actions, .news, .events]
+    
+    private let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    @State private var path: [HomeSectionType] = []
+    
+    var body: some View {
+        NavigationStack(path: $path) {
+            GeometryReader { geo in
+                LazyVGrid(columns: columns) {
+                    ForEach(homeSections, id: \.self) { section in
+                                            Button {
+                                                path.append(section)
+                                            } label: {
+                                                CircleIconView(iconName: section.icon, title: section.title)
+                                            }
+                                            .buttonStyle(.borderless)
+                                        }
+                }
+                .frame(width: geo.size.width, height: geo.size.height)
+                .background(Color.white)
+            }
+        }
+        
+        .navigationDestination(for: HomeSectionType.self) { section in
+            switch section {
+            case .timeSheet:
+                SpashView()
+            case .actions:
+                SpashView()
+            case .news:
+                SpashView()
+            case .events:
+                SpashView()
+            }
+        }
+    }
+}
+
+struct CircleIconView: View {
+    let iconName: String
+    let title: String
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            Image(iconName)
+                .resizable()
+                .scaledToFit()
+                .clipShape(Circle())
+                .frame(width: 50, height: 50)
+                .background(Color.clear)
+            Text(title)
+                .foregroundColor(.black)
+                .font(.footnote).background(Color.clear)
+        }
+    }
+}
+
