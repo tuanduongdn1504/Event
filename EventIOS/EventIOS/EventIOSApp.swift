f5abffcc7f9d44424fedbec9ad2ff3fad513b760
Duong Tuan <tuan.duong@enouvo.com>
2025-07-09 15:27:32 +0700

    init: New iOS application

---
+++ EventIOS/EventIOS/EventIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventIOSApp.swift
+//  EventIOS
+//
+//  Created by Duong Tuan on 01/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct EventIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
